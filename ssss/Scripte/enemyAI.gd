686232e1cf80d0e0de498ef379e0719cc7f49630
JoJo <jonathan.buss.aka.jo@gmail.com>
2024-11-16 16:42:21 +0100

    assets

--- ssss/Scripte/enemyAI.gd
+++ ssss/Scripte/enemyAI.gd
@@ -6,7 +6,16 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var facing_right = true
 
+enum state {CAGE,FLEETING,ANGRY, ATTACKING}
+var currentState = state.CAGE
+
 func _physics_process(delta):
+	if currentState == state.CAGE:
+		miseryInCage(delta)
+	move_and_slide()
+
+
+func miseryInCage(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	#if $GroundDetectorLeft.is_colliding():
@@ -20,4 +29,3 @@ func _physics_process(delta):
 		scale.x = abs(scale.x) * -1
 		
 	velocity.x = speed
-	move_and_slide()
