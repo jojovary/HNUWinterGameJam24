d69eab7fa77036a97e17d7f9e715d3d90097214d
JoJo <jonathan.buss.aka.jo@gmail.com>
2024-11-17 01:40:34 +0100

    bloodsplatter

---
+++ ssss/Scripte/bloddspawn.gd
@@ -0,0 +1,26 @@
+extends Node2D
+var bloodsplatter = preload("res://Szenen/bloodsplatter_side_flipped.tscn")
+var bloodsplatter2 = preload("res://Szenen/bloodsplater_up_flipped.tscn")
+var muzzlePositions = []
+
+
+# adds the muzzlePositions into the array
+func _ready():
+	for i in get_children():
+		muzzlePositions.append(i)
+
+# handle inputs
+func blood():
+	for i in muzzlePositions:
+		var rando = randi_range(0,3)
+		if rando < 3:
+			var blood = bloodsplatter.instantiate()
+			get_tree().current_scene.add_child(blood)
+			i.global_position.y =  i.global_position.y + randf_range(-1,3)
+			blood.global_position = i.global_position
+		else:
+			var blood = bloodsplatter2.instantiate()
+			get_tree().current_scene.add_child(blood)
+			i.global_position.y =  i.global_position.y + randf_range(-2,2)
+			blood.global_position = i.global_position
+		
